274d891cb34067c488e517637c49c0c8d0a92a04
HanLu <146564137+HanLu-ai@users.noreply.github.com>
2025-06-28 17:59:32 +0800

    Add files via upload

---
+++ ArithmeticOperators/ArithmeticOperators/src/ArithmeticOperators.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用御风工作室教程");
+        System.out.println(10 / 3);
+        System.out.println(10.0 / 3);
+        System.out.println(10 % 2);
+        System.out.println(10 % 3);
+        System.out.println(15 % 2);
+    }
+}
